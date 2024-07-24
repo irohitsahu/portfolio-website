24304bf5c381958caded493b53860fe3ca459588
Rohit Sahu <irohitsahu97@gmail.com>
2024-07-24 23:18:41 +0530

    ui fixes

--- public/scripts/pageLoad.js
+++ public/scripts/pageLoad.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   window.scrollTo(0, 0);
+  typeWelcome();
   console.log("Page loaded and scrolled to top");
 };
 
@@ -29,4 +30,33 @@ function openSidebar() {
   } else {
     sidebar.classList.add("active");
   }
+}
+
+function typeWelcome() {
+  const content = `
+      Hey there!👋<br>
+      I'm Rohit Sahu<br>
+      A front-end developer with expertise in transforming design visions into interactive experiences.<br>
+      Welcome to my portfolio! 🚀✨
+      `;
+
+  const welcomeMsg = document.querySelector(".welcome-msg");
+  let index = 0;
+
+  function typeWriter() {
+    if (content.slice(index, index + 4) === "<br>") {
+      welcomeMsg.innerHTML += "<br>";
+      index += 4;
+    } else {
+      welcomeMsg.innerHTML += content.charAt(index);
+      index++;
+    }
+
+    if (index < content.length) {
+      setTimeout(typeWriter, 30); // Adjust typing speed here (in milliseconds)
+    }
+  }
+
+  // Start the typewriting effect
+  typeWriter();
 }
