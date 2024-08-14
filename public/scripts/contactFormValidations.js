81e5466320a2def09754919508999a168e0303a3
Rohit Sahu <irohitsahu97@gmail.com>
2024-08-14 22:45:37 +0530

    messgae form added

--- public/scripts/contactFormValidations.js
+++ public/scripts/contactFormValidations.js
@@ -24,6 +24,7 @@ function handleFormInputChange(e) {
     ...error,
     [name]: null,
   };
+  renderErrors();
 }
 
 function validateInput() {
@@ -38,12 +39,59 @@ function validateInput() {
   return newEror;
 }
 
-function handelFormSubmit(e) {
-  console.log("submit clicked");
+function renderErrors() {
+  for (let name in error) {
+    const errorElement = document.querySelector(`.error-msg[name=${name}]`);
+    if (error[name]) {
+      errorElement.textContent = error[name];
+      errorElement.style.display = "inline";
+    } else {
+      errorElement.textContent = "";
+      errorElement.style.display = "none";
+    }
+  }
+}
+
+async function handleFormSubmit(e) {
   e.preventDefault();
 
   const validatedError = validateInput();
   if (Object.keys(validatedError).length > 0) {
     error = validatedError;
+    renderErrors();
+  } else {
+    const formData = new FormData(e.target);
+    formData.append("access_key", "4b94d08b-901f-4837-bf4b-2b8496cabc74");
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+    if (res.success) {
+      console.log("Success", res);
+      alert(
+        "Your message has been sent successfully. You will be contacted via the provided email or mobile number."
+      );
+    }
+
+    defaultFormValue = {
+      name: "",
+      email: "",
+      mobile: "",
+      message: "",
+    };
+    error = {
+      name: null,
+      email: null,
+      mobile: null,
+      message: null,
+    };
+    renderErrors();
+    document.querySelector(".contact-me-form").reset();
   }
 }
